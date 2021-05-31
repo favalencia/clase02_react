fe3055d28b380da489645dc4861f83376c314efe
Felipe Valencia <felipe.valencia@hqb.cl>
2021-05-31 16:10:30 -0400

    Correccion

--- src/container/home/Home.tsx
+++ src/container/home/Home.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import {useState} from 'react'
 import Header from '../../components/header/Header'
 import Footer from '../../components/footer/Footer'
 import Menu from '../../components/menu/Menu'
